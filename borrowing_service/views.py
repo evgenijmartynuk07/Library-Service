d4e67f3e45b7376ab3a6b0b9b8532640a388a6e7
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2023-05-06 15:51:56 +0300

    Create borrowing view with change inventory book

--- borrowing_service/views.py
+++ borrowing_service/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins, status
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
+from django.db import transaction
 
-# Create your views here.
+from books_service.models import Book
+from borrowing_service.models import Borrowing
+from borrowing_service.serializers import BorrowingListSerializer, BorrowingDetailSerializer, BorrowingCreateSerializer
+
+
+class BorrowingViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    permission_classes = (IsAuthenticated,)
+    queryset = Borrowing.objects.select_related("book", "user")
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        return BorrowingListSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+        data['user'] = request.user.id
+        book = Book.objects.get(id=request.data.get("book"))
+        if book.inventory <= 0:
+            raise "No book available"
+        with transaction.atomic():
+            book.inventory -= 1
+            book.save()
+            serializer = self.get_serializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED, headers=headers
+            )
