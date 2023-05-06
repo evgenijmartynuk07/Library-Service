c609823babbe6102bdc249c1f4af0f9d75f62416
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2023-05-06 09:26:41 +0300

    Start customers_cervice app

--- books_service/models.py
+++ books_service/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    COVER_CHOICES = (
+        ("S", "SOFT"),
+        ("H", "HARD"),
+    )
+
+    title = models.CharField(max_length=128)
+    author = models.CharField(max_length=256)
+    cover = models.CharField(max_length=8, choices=COVER_CHOICES)
+    inventory = models.PositiveIntegerField(default=0)
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self) -> str:
+        return f"name: {self.title}, author: {self.author}"
