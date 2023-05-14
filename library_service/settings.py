ec8d594a12769ca480f9223b38435195f8361ce1
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2023-05-14 12:24:23 +0300

    Add tests for Book

--- library_service/settings.py
+++ library_service/settings.py
@@ -9,20 +9,20 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-%q9rd9kj3n6(vh$+bwa59d&lwhw!l5q5@#+a0!!qfxgq+xsv2*"
-)
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
